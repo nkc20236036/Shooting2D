c858139a981e75e6c634ef6f82240db1e6323905
nkc20236036 <nkc23.mizutani.ryutaro@gmail.com>
2023-06-06 15:10:09 +0900

    6/6更新

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 dir = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 5;
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+
+        transform.position += dir.normalized * speed * Time.deltaTime;
+        Vector3 pos = transform.position;
+         pos.x = Mathf.Clamp(pos.x, -9f, 9f);
+        pos.y = Mathf.Clamp(pos.y, -5f, 5f);
+        transform.position = pos;
+
+    }
+}
