8783bf62ef11d38bafa73bef6048773eaf1b1673
nkc20236036 <nkc23.mizutani.ryutaro@gmail.com>
2023-06-23 11:09:30 +0900

    6/23 更新

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,77 +1,97 @@
+//----------------------------------------------------------------------------------------
+// 科目：ゲームプログラミング
+// 内容：プレーヤーの制御スクリプト
+// 担当：Ken.D.Ohishi 2023.06.06
+//----------------------------------------------------------------------------------------
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
 
 public class PlayerController : MonoBehaviour
 {
-    Vector3 dir = Vector3.zero;
-    GameObject ShotPre;
-    float timer;
-    int power = 0;
-    Animator anim;
-    float speed;
-    public GameObject nannkoitu;
+    Vector3 dir;    // 移動方向保存
+    float speed;    // 移動速度保存
+    float timer;    // 自弾の発射間隔計算用
+    Animator anm;   // アニメーターコンポーネントを保存
 
-    // Start is called before the first frame update
-    void Start()
+    public GameObject shotPre; // 弾のプレハブをセット
+
+    // 自キャラのスピードの値を他のスクリプトから
+    // 参照・変更するためのプロパティ
+    public float Speed
     {
-        
-        anim = GetComponent<Animator>();
-        ShotPre = (GameObject)Resources.Load("ShotPre");
+        set
+        {
+            speed = value;
+            speed = Mathf.Clamp(speed, 1, 20);
+        }
+        get { return speed; }
     }
 
-    // Update is called once per frame
+    int shotLevel;  // 武器のレベル
+    public int ShotLevel
+    {
+        set 
+        { 
+            shotLevel = value;
+            shotLevel = Mathf.Clamp(shotLevel, 0, 12);
+        }
+        get { return shotLevel; }
+    }
+
+    void Start()
+    {
+        // アニメーターコンポーネント取得
+        anm = GetComponent<Animator>(); 
+        shotLevel = 0;  // 弾レベル
+        timer     = 0;  // 時間初期化
+        speed     = 10; // 初期スピード
+    }
+
     void Update()
     {
-        float speed = 10;
-        Vector3 dir = Vector3.zero;
+        // 上下左右移動
         dir.x = Input.GetAxisRaw("Horizontal");
         dir.y = Input.GetAxisRaw("Vertical");
-
         transform.position += dir.normalized * speed * Time.deltaTime;
-        Vector3 pos = transform.position;
+
+        // CキーでshotLevel変更(デバッグ用)
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            shotLevel = (shotLevel + 1) % 13;
+        }
+
+        // Zキーが押されているとき弾を発射
+        timer += Time.deltaTime;
+        if (timer >= 0.3f && Input.GetKey(KeyCode.Z))
+        {
+            timer = 0;
+            shotLevel = (shotLevel < 0) ? 0 : shotLevel;
+            for (int i = -shotLevel; i < shotLevel + 1; i++)
+            {
+                // 弾の生成位置はプレーヤーと同じ場所
+                Vector3 p = transform.position;
+
+                // プレーヤーの回転角度を取得し、15度ずつずらした方向に弾を回転させる
+                //Vector3 r = transform.rotation.eulerAngles + new Vector3(0, 0, 15f * i);
+                //Quaternion rot = Quaternion.Euler(r);
+                Quaternion rot = Quaternion.identity;
+                rot.eulerAngles = transform.rotation.eulerAngles + new Vector3(0, 0, 15f * i);
+
+                // 位置と回転情報をセットして生成
+                Instantiate(shotPre, p, rot);
+            }
+        }
+
+        // 画面内制限
+        Vector2 pos = transform.position;
         pos.x = Mathf.Clamp(pos.x, -9f, 9f);
         pos.y = Mathf.Clamp(pos.y, -5f, 5f);
         transform.position = pos;
 
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            Instantiate(nannkoitu);
-        }
-        
-            timer = 0;
-
-            if (dir.y == 0)
-            {
-                anim.Play("Player");
-            }
-            else if (dir.y == 1)
-            {
-                anim.Play("Player L");
-            }
-            else if (dir.y == -1)
-            {
-                anim.Play("PlayerR");
-            }
-       
-        timer += Time.deltaTime;
-            if (Input.GetKey(KeyCode.Z) && timer > 0.3f)
-            {
-
-
-                for (int i = -power; i < power + 1; i++)
-                {
-                    Vector3 po = transform.position + new Vector3(0, 0.5f, 0);
-
-                    Vector3 r = transform.rotation.eulerAngles + new Vector3(0, 15f * i, 0);
-                    Quaternion rot = Quaternion.Euler(r);
-
-
-                    Instantiate(ShotPre, pos, rot);
-                }
-                timer = 0;
-            }
-        }
+        // アニメーション設定
+        if (dir.y == 0)         anm.Play("neutral");
+        else if (dir.y == 1)    anm.Play("LMove");
+        else if (dir.y == -1)   anm.Play("RMove");       
     }
+}
